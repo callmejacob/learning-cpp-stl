e0aeb05eb6f3372e57796c86ddd63b3fb2114b1d
callmejacob <callmejacob@foxmail.com>
2023-06-05 10:57:29 +0800

    chore: add iterator.

--- iterator.cpp
+++ iterator.cpp
@@ -1,5 +1,6 @@
 #include "iterator.h"
 
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -20,7 +21,48 @@ static void TestIterator_1(int argc, const char **argv) {
   cout << "[iterator] test 1 end." << endl;
 }
 
+static void TestIterator_2() {
+  cout << "[iterator] test 2 begin." << endl;
+
+  vector<int> v;
+  copy(istream_iterator<int>(cin), istream_iterator<int>(), back_inserter(v));
+
+  copy(v.begin(),v.end(), ostream_iterator<int>(cout, " "));
+  cout << endl;
+
+  cout << "[iterator] test 2 end." << endl;
+}
+
+static void TestIterator_3() {
+  // good.txt
+  // 3
+  // 4
+  // 5
+  // 6
+  // 7
+
+  cout << "[iterator] test 3 begin." << endl;
+  ifstream ifs("good.txt");
+  int tmp;
+  while (ifs >> tmp) {
+    cout << tmp << endl;
+  }
+  cout << "[iterator] test 3 end." << endl;
+
+  cout << "[iterator] 3 copy begin." << endl;
+  ifstream ifs2("good.txt");
+  copy(istream_iterator<int>(ifs2), istream_iterator<int>(), ostream_iterator<int>(cout, " "));
+  cout << endl;
+  cout << "[iterator] 3 copy end." << endl;
+}
+
 void TestIterator() {
   const char *good[] = {"program", "argc_1", "argc_2", "argc_3"};
   TestIterator_1(4, good);
+
+  // disable for cin
+//  TestIterator_2();
+
+  // disable for file
+//  TestIterator_3();
 }
