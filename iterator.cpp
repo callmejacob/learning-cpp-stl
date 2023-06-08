33c57b57cece8c36ac56ae3e7f5f91f26b3d9671
callmejacob <callmejacob@foxmail.com>
2023-06-08 14:49:24 +0800

    chore: bidirectional iterator.

--- iterator.cpp
+++ iterator.cpp
@@ -2,6 +2,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <string>
 #include <vector>
 
@@ -85,6 +86,46 @@ static void TestIterator_4() {
   cout << endl;
 }
 
+template<typename BiDirectoryIterator, typename Compare>
+static void BubbleSort(BiDirectoryIterator first, BiDirectoryIterator last, Compare comp) {
+  BiDirectoryIterator left_it = first;
+  BiDirectoryIterator right_it = first;
+  ++right_it;
+  while (first != last) {
+    while (right_it != last) {
+      if (comp(*left_it, *right_it)) {
+        swap(*left_it, *right_it);
+      }
+
+      ++right_it;
+      ++left_it;
+    }
+
+    last--;
+    left_it = first;
+    right_it = first;
+    ++right_it;
+  }
+}
+
+void TestIterator_5() {
+  cout << "[iterator] 5 begin." << endl;
+
+  list<int> good = {1, 5, 2, 6, 7, 9, 3};
+  cout << "[iterator] 5 before bubble sort." << endl;
+  copy(good.begin(), good.end(), ostream_iterator<int>(cout, " "));
+  cout << endl;
+
+  BubbleSort(good.begin(), good.end(), [](int left, int right) {
+    return left <= right;
+  });
+
+  cout << "[iterator] 5 after bubble sort." << endl;
+  copy(good.begin(), good.end(), ostream_iterator<int>(cout, " "));
+  cout << endl;
+  cout << "[iterator] 5 end." << endl;
+}
+
 void TestIterator() {
   const char *good[] = {"program", "argc_1", "argc_2", "argc_3"};
   TestIterator_1(4, good);
@@ -96,4 +137,5 @@ void TestIterator() {
 //  TestIterator_3();
 
   TestIterator_4();
+  TestIterator_5();
 }
