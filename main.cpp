889f9fb9e31433a8d0b200ea6ca1ef09443d1d09
callmejacob <callmejacob@foxmail.com>
2023-06-15 17:07:12 +0800

    chore: recogonize folder.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
-#include "qsort.h"
-#include "search.h"
-#include "vector.h"
-#include "deque.h"
-#include "list.h"
-#include "set.h"
-#include "map.h"
-#include "multiset.h"
+#include "algorithm/qsort.h"
+#include "algorithm/search.h"
+#include "container/vector.h"
+#include "container/deque.h"
+#include "container/list.h"
+#include "container/set.h"
+#include "container/map.h"
+#include "container/multiset.h"
 
-#include "condition.h"
+#include "other/condition.h"
 
-#include "iterator.h"
+#include "iterator/iterator.h"
 
-#include "str64.h"
+#include "other/str64.h"
 
 int main() {
   TestQsort();
