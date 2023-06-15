1d7e56a1d57d2a3c6a4f9991f7068a5e8f387943
callmejacob <callmejacob@foxmail.com>
2023-06-15 18:10:09 +0800

    chore: add remove_if algorithm.

--- algorithm/remove.cpp
+++ algorithm/remove.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void TestRemove() {
+static void TestRemove1() {
   vector<int> v = {1, 2, 3, 4, 5};
 
   cout << "[remove] before:" << endl;
@@ -18,4 +18,45 @@ void TestRemove() {
   cout << "[remove] after:" << endl;
   copy(v.begin(), te, ostream_iterator<int>(cout, ","));
   cout << endl;
+}
+
+struct CheckOd {
+  bool operator()(int v) {
+    return v % 2 == 0;
+  }
+};
+
+static void TestRemove2() {
+  vector<int> v = {1, 2, 3, 4, 5};
+
+  cout << "[remove] 2, before:" << endl;
+  copy(v.begin(), v.end(), ostream_iterator<int>(cout, ","));
+  cout << endl;
+
+  auto te = remove_if(v.begin(), v.end(), CheckOd());
+
+  cout << "[remove] 2, after:" << endl;
+  copy(v.begin(), te, ostream_iterator<int>(cout, ","));
+  cout << endl;
+}
+
+static void TestRemove3() {
+  vector<int> v = {1, 2, 3, 4, 5};
+
+  cout << "[remove] 2, before:" << endl;
+  copy(v.begin(), v.end(), ostream_iterator<int>(cout, ","));
+  cout << endl;
+
+  auto te2 = remove_if(v.begin(), v.end(), [](int v){
+    return v % 2 == 1;
+  });
+  cout << "[remove] 2, after2:" << endl;
+  copy(v.begin(), te2, ostream_iterator<int>(cout, ","));
+  cout << endl;
+}
+
+void TestRemove() {
+  TestRemove1();
+  TestRemove2();
+  TestRemove3();
 }
